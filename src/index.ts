a307acfdb11e7006895e8fe8f15c800c5183abb6
Andrea Vallotti <andrea.vallotti@gmail.com>
2024-07-12 17:05:03 +0200

    run codegen

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,11 @@
-/**
- * @since 0.0.1
- */
+
+export * as Collection from "./Collection.js"
+
+
+export * as Db from "./Db.js"
+
+
 export * as MongoClient from "./MongoClient.js"
+
+
+export * as MongoError from "./MongoError.js"
