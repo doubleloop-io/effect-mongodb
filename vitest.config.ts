d2a7ab67c40ea3842a5f00a42be96769ff08d363
Cosimo Matteini <dev.matteini@gmail.com>
2024-07-19 15:35:16 +0200

    format vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,15 +2,15 @@ import * as path from "node:path"
 import { defineConfig } from "vitest/config"
 
 export default defineConfig({
-    test: {
-        include: ["test/**/*.{test,spec}.?(c|m)[jt]s?(x)"],
-        reporters: ["hanging-process", "default"],
-        sequence: {
-            concurrent: true,
-        },
-        alias: {
-            "@doubleloop-io/effect-mongodb": path.join(__dirname, "src"),
-            "@doubleloop-io/effect-mongodb/test": path.join(__dirname, "test"),
-        },
+  test: {
+    include: ["test/**/*.{test,spec}.?(c|m)[jt]s?(x)"],
+    reporters: ["hanging-process", "default"],
+    sequence: {
+      concurrent: true
     },
+    alias: {
+      "@doubleloop-io/effect-mongodb": path.join(__dirname, "src"),
+      "@doubleloop-io/effect-mongodb/test": path.join(__dirname, "test")
+    }
+  }
 })
