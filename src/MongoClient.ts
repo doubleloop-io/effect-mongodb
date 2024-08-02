7504de6dfc83c207193deb41b57722db42026636
Andrea Vallotti <andrea.vallotti@gmail.com>
2024-08-02 15:32:59 +0200

    connect - add tag to MongoError

--- src/MongoClient.ts
+++ src/MongoClient.ts
@@ -5,11 +5,14 @@ import * as Effect from "effect/Effect"
 import * as F from "effect/Function"
 import type { Db, DbOptions, MongoClientOptions } from "mongodb"
 import { MongoClient as MongoClient_ } from "mongodb"
+import * as MongoError from "./MongoError.js"
 
 export type MongoClient = MongoClient_
 
 export const connect = (url: string, options?: MongoClientOptions) =>
-  Effect.promise(() => MongoClient_.connect(url, options))
+  Effect.promise(() => MongoClient_.connect(url, options)).pipe(
+    Effect.catchAll(MongoError.mongoErrorDie<ReadonlyArray<unknown>>("connect error"))
+  )
 
 export const db: {
   (dbName?: string, options?: DbOptions): (client: MongoClient) => Effect.Effect<Db>
