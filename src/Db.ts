de2cdf8089003f57d16d7b35c204030a01776b98
Andrea Vallotti <andrea.vallotti@gmail.com>
2024-09-27 17:41:27 +0200

    add service tags for MongoClient and Db

--- src/Db.ts
+++ src/Db.ts
@@ -1,10 +1,13 @@
 /**
  * @since 0.0.1
  */
+import type * as Brand from "effect/Brand"
+import * as Context from "effect/Context"
 import * as Effect from "effect/Effect"
 import * as F from "effect/Function"
 import type { Db } from "mongodb"
 import * as DocumentCollection from "./DocumentCollection.js"
+import type * as MongoError from "./MongoError.js"
 
 export const collection: {
   (name: string): (db: Db) => Effect.Effect<DocumentCollection.DocumentCollection>
@@ -15,3 +18,9 @@ export const collection: {
       collection: db.collection(name)
     })
   ))
+
+export type DbService<K extends string> = {
+  db: Effect.Effect<Db, MongoError.MongoError>
+} & Brand.Brand<K>
+
+export const Tag = <K extends string>(key: K) => Context.GenericTag<DbService<K>>(key)
--- src/MongoClient.ts
+++ src/MongoClient.ts
@@ -1,11 +1,14 @@
 /**
  * @since 0.0.1
  */
+import type * as Brand from "effect/Brand"
 import * as Effect from "effect/Effect"
 import * as F from "effect/Function"
 import type { Db, DbOptions, MongoClientOptions } from "mongodb"
 import { MongoClient as MongoClient_ } from "mongodb"
 import * as MongoError from "./MongoError.js"
+
+import * as Context from "effect/Context"
 
 export type MongoClient = MongoClient_
 
@@ -21,3 +24,9 @@ export const db: {
   3,
   (client: MongoClient, dbName?: string, options?: DbOptions) => Effect.sync(() => client.db(dbName, options))
 )
+
+export type MongoClientService<K extends string> = {
+  client: Effect.Effect<MongoClient, MongoError.MongoError>
+} & Brand.Brand<K>
+
+export const Tag = <K extends string>(key: K) => Context.GenericTag<MongoClientService<K>>(key)
