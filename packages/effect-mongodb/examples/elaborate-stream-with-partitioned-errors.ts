1ef4850ee6529351055c33bbcf67cdeed02ce746
Cosimo Matteini <dev.matteini@gmail.com>
2025-01-02 15:05:52 +0100

    examples: elaborate-stream-with-partitioned-errors is now runnable (#12)

--- packages/effect-mongodb/examples/elaborate-stream-with-partitioned-errors.ts
+++ packages/effect-mongodb/examples/elaborate-stream-with-partitioned-errors.ts
@@ -14,12 +14,26 @@ const MyType = Schema.Struct({
   age: Schema.Number,
   birthday: Schema.Date
 })
-type MyType = Schema.Schema.Type<typeof MyType>
+type MyType = typeof MyType.Type
+
+const mongoClient = (url: string) =>
+  Effect.acquireRelease(
+    MongoClient.connect(url),
+    (client) => MongoClient.close(client).pipe(Effect.orDie)
+  )
 
 const program = Effect.gen(function*(_) {
-  const sourceInstance = yield* _(MongoClient.connect("mongodb://localhost:27017"))
-  const sourceDb = MongoClient.db(sourceInstance, "source")
+  const sourceInstance = yield* _(mongoClient("mongodb://localhost:27017"))
+  const sourceDb = MongoClient.db(sourceInstance, "elaborate-stream-with-partitioned-errors")
   const sourceCollection = Db.documentCollection(sourceDb, "records")
+  yield* _(DocumentCollection.insertMany(sourceCollection, [
+    { name: "User 1", age: 30, birthday: "1994-03-10T00:00:00Z" },
+    { name: "User 2", age: "24", birthday: "2000-04-25T00:00:00Z" },
+    { name: "User 3", age: 29, birthday: "802828800000" },
+    { name: "User 4", age: 80, birthday: "1944-07-21T00:00:00Z" },
+    { name: "User 5", age: 4, birthday: "2020-11-03T00:00:00Z" },
+    { name: "User 6", age: 30, birthday: "19940310T000000Z" }
+  ]))
 
   yield* _(
     DocumentCollection.find(sourceCollection),
@@ -31,11 +45,11 @@ const program = Effect.gen(function*(_) {
           ParseResult.TreeFormatter.formatError(error).pipe(
             Effect.flatMap((error) => Effect.logError(`Unable to decode item`, { document, error }))
           ),
-        onRight: (x: MyType) => Effect.log(`Elaborate ${x}`)
+        onRight: (x) => Effect.log(`Elaborated ${x.name}`)
       })
     ),
     Stream.runDrain
   )
 })
 
-await program.pipe(Effect.runPromise)
+await program.pipe(Effect.scoped, Effect.runPromise)
