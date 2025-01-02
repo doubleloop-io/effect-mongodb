579d2a0bb17ad76a4dddca01f5e1fdf479936db3
Cosimo Matteini <dev.matteini@gmail.com>
2025-01-02 14:47:52 +0100

    examples: copy-between-dbs is now runnable (#12)

--- packages/effect-mongodb/examples/copy-between-dbs.ts
+++ packages/effect-mongodb/examples/copy-between-dbs.ts
@@ -12,23 +12,35 @@ const MyType = Schema.Struct({
   age: Schema.Number,
   birthday: Schema.Date
 })
-type MyType = Schema.Schema.Type<typeof MyType>
+type MyType = typeof MyType.Type
+
+const mongoClient = (url: string) =>
+  Effect.acquireRelease(
+    MongoClient.connect(url),
+    (client) => MongoClient.close(client).pipe(Effect.orDie)
+  )
 
 const program = Effect.gen(function*(_) {
-  const sourceInstance = yield* _(MongoClient.connect("mongodb://localhost:27017"))
+  const sourceInstance = yield* _(mongoClient("mongodb://localhost:27017"))
   const sourceDb = MongoClient.db(sourceInstance, "source")
   const sourceCollection = Db.documentCollection(sourceDb, "records")
+  yield* _(DocumentCollection.insertMany(sourceCollection, [
+    { name: "User 1", age: 30, birthday: "1994-03-10T00:00:00Z" },
+    { name: "User 2", age: 80, birthday: "1944-07-21T00:00:00Z" },
+    { name: "User 3", age: 4, birthday: "2020-11-03T00:00:00Z" }
+  ]))
+
   const sourceItems = yield* _(
     DocumentCollection.find(sourceCollection),
     DocumentFindCursor.typed(MyType),
     FindCursor.toArray
   )
 
-  const destinationInstance = yield* _(MongoClient.connect("mongodb://localhost:27017"))
+  const destinationInstance = yield* _(mongoClient("mongodb://localhost:27017"))
   const destinationDb = MongoClient.db(destinationInstance, "destination")
   const destinationCollection = Db.collection(destinationDb, "records", MyType)
 
   yield* _(Collection.insertMany(destinationCollection, sourceItems))
 })
 
-await program.pipe(Effect.runPromise)
+await program.pipe(Effect.scoped, Effect.runPromise)
