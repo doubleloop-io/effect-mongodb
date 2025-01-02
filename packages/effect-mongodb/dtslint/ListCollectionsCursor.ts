1d4b58b0846b1e0d1749a70a5a4a43462b90600a
Cosimo Matteini <dev.matteini@gmail.com>
2025-01-02 17:52:54 +0100

    ListCollectionsCursor: add type-level tests (#26)

---
+++ packages/effect-mongodb/dtslint/ListCollectionsCursor.ts
@@ -0,0 +1,17 @@
+import * as ListCollectionsCursor from "effect-mongodb/ListCollectionsCursor"
+
+declare const cursor: ListCollectionsCursor.ListCollectionsCursor<ListCollectionsCursor.FullCollectionInfo>
+
+// -------------------------------------------------------------------------------------
+// toArray
+// -------------------------------------------------------------------------------------
+
+// $ExpectType Effect<readonly CollectionInfo[], MongoError, never>
+ListCollectionsCursor.toArray(cursor)
+
+// -------------------------------------------------------------------------------------
+// toStream
+// -------------------------------------------------------------------------------------
+
+// $ExpectType Stream<CollectionInfo, MongoError, never>
+ListCollectionsCursor.toStream(cursor)
