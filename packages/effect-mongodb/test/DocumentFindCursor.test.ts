37284842dc951333fb450aedc9d0574f21dde6bc
Cosimo Matteini <dev.matteini@gmail.com>
2025-01-10 17:48:26 +0100

    DocumentFindCursor: review tests (#31)

--- packages/effect-mongodb/test/DocumentFindCursor.test.ts
+++ packages/effect-mongodb/test/DocumentFindCursor.test.ts
@@ -8,136 +8,12 @@ import { expect, test } from "vitest"
 import { describeMongo } from "./support/describe-mongo.js"
 
 describeMongo("DocumentFindCursor", (ctx) => {
-  test("filter", async () => {
-    const program = Effect.gen(function*(_) {
-      const db = yield* _(ctx.database)
-      const collection = Db.documentCollection(db, "filter")
-
-      yield* _(
-        DocumentCollection.insertMany(collection, [
-          { id: 1, type: "User" },
-          { id: 2, type: "Admin" },
-          { id: 3, type: "User" },
-          { id: 4, type: "Admin" }
-        ])
-      )
-
-      return yield* _(
-        DocumentCollection.find(collection),
-        DocumentFindCursor.filter({ type: "Admin" }),
-        DocumentFindCursor.toArray
-      )
-    })
-
-    const result = await Effect.runPromise(program)
-
-    expect(result).toEqual([
-      expect.objectContaining({ id: 2, type: "Admin" }),
-      expect.objectContaining({ id: 4, type: "Admin" })
-    ])
-  })
-
-  test("project", async () => {
-    const now = new Date("2024-07-26T09:30:00.000Z")
-
-    const program = Effect.gen(function*(_) {
-      const db = yield* _(ctx.database)
-      const collection = Db.documentCollection(db, "project")
-
-      yield* _(
-        DocumentCollection.insertMany(collection, [
-          { id: 1, type: "User" },
-          { id: 2, type: "Admin" },
-          { id: 3, type: "User" }
-        ])
-      )
-
-      return yield* _(
-        DocumentCollection.find(collection),
-        DocumentFindCursor.project({ _id: 0, id: 1, type: 1, createdTime: now }),
-        DocumentFindCursor.toArray
-      )
-    })
-
-    const result = await Effect.runPromise(program)
-
-    expect(result).toEqual([
-      { id: 1, type: "User", createdTime: now },
-      { id: 2, type: "Admin", createdTime: now },
-      { id: 3, type: "User", createdTime: now }
-    ])
-  })
-
-  test("sort", async () => {
-    const program = Effect.gen(function*(_) {
-      const db = yield* _(ctx.database)
-      const collection = Db.documentCollection(db, "sort")
-
-      yield* _(
-        DocumentCollection.insertMany(collection, [
-          { id: 4 },
-          { id: 2 },
-          { id: 5 },
-          { id: 3 },
-          { id: 1 }
-        ])
-      )
-
-      return yield* _(
-        DocumentCollection.find(collection),
-        DocumentFindCursor.sort({ id: 1 }),
-        DocumentFindCursor.toArray
-      )
-    })
-
-    const result = await Effect.runPromise(program)
-
-    expect(result).toEqual([
-      expect.objectContaining({ id: 1 }),
-      expect.objectContaining({ id: 2 }),
-      expect.objectContaining({ id: 3 }),
-      expect.objectContaining({ id: 4 }),
-      expect.objectContaining({ id: 5 })
-    ])
-  })
-
-  test("limit", async () => {
-    const program = Effect.gen(function*(_) {
-      const db = yield* _(ctx.database)
-      const collection = Db.documentCollection(db, "limit")
-
-      yield* _(
-        DocumentCollection.insertMany(collection, [
-          { id: 1 },
-          { id: 2 },
-          { id: 3 },
-          { id: 4 },
-          { id: 5 }
-        ])
-      )
-
-      return yield* _(
-        DocumentCollection.find(collection),
-        DocumentFindCursor.limit(3),
-        DocumentFindCursor.toArray
-      )
-    })
-
-    const result = await Effect.runPromise(program)
-
-    expect(result).toEqual([
-      expect.objectContaining({ id: 1 }),
-      expect.objectContaining({ id: 2 }),
-      expect.objectContaining({ id: 3 })
-    ])
-  })
-
-  test("many operations", async () => {
+  test("acceptance test", async () => {
     const beforeJuly = "2024-07-01T00:00:00.000Z"
 
     const program = Effect.gen(function*(_) {
       const db = yield* _(ctx.database)
-      const collection = Db.documentCollection(db, "many-operations")
+      const collection = Db.documentCollection(db, "acceptance-test")
 
       yield* _(
         DocumentCollection.insertMany(collection, [
@@ -153,7 +29,7 @@ describeMongo("DocumentFindCursor", (ctx) => {
         DocumentCollection.find(collection),
         DocumentFindCursor.filter({ createdOn: { $lt: beforeJuly } }),
         DocumentFindCursor.sort("createdOn", "ascending"),
-        DocumentFindCursor.project({ _id: 0, createdOn: 0 }),
+        DocumentFindCursor.project({ _id: 0, id: 1 }),
         DocumentFindCursor.limit(2),
         DocumentFindCursor.toArray
       )
@@ -161,10 +37,7 @@ describeMongo("DocumentFindCursor", (ctx) => {
 
     const result = await Effect.runPromise(program)
 
-    expect(result).toEqual([
-      expect.objectContaining({ id: 1 }),
-      expect.objectContaining({ id: 5 })
-    ])
+    expect(result).toEqual([{ id: 1 }, { id: 5 }])
   })
 
   test("stream", async () => {
