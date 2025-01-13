b73d03047684be38b1e21857e4d6ef8bfe798e1c
Andrea Vallotti <andrea.vallotti@gmail.com>
2025-01-13 11:55:25 +0100

    remove unused code (#17)

--- packages/effect-mongodb/src/MongoError.ts
+++ packages/effect-mongodb/src/MongoError.ts
@@ -2,11 +2,7 @@
  * @since 0.0.1
  */
 import * as Data from "effect/Data"
-import * as Effect from "effect/Effect"
-import * as F from "effect/Function"
-import * as Match from "effect/Match"
-import * as Stream from "effect/Stream"
-import { MongoError as MongoError_ } from "mongodb"
+import type { MongoError as MongoError_ } from "mongodb"
 
 export class ClientErrorSource extends Data.TaggedClass("ClientErrorSource")<{
   module: string
@@ -34,23 +30,3 @@ export class MongoError extends Data.TaggedError("MongoError")<{
   cause: MongoError_
   source?: ErrorSource
 }> {}
-
-export const mongoErrorDie: {
-  <A>(message: string): (error: unknown) => Effect.Effect<A, MongoError>
-  <A>(error: unknown, message: string): Effect.Effect<A, MongoError>
-} = F.dual(2, <A>(error: unknown, message: string): Effect.Effect<A, MongoError> =>
-  F.pipe(
-    Match.value(error),
-    Match.when(Match.instanceOf(MongoError_), (cause) => Effect.fail(new MongoError({ message, cause }))),
-    Match.orElse((cause) => Effect.die(new Error(message, { cause })))
-  ))
-
-export const mongoErrorStream: {
-  <A>(message: string): (error: unknown) => Stream.Stream<A, MongoError>
-  <A>(error: unknown, message: string): Stream.Stream<A, MongoError>
-} = F.dual(2, <A>(error: unknown, message: string): Stream.Stream<A, MongoError> =>
-  F.pipe(
-    Match.value(error),
-    Match.when(Match.instanceOf(MongoError_), (cause) => Stream.fail(new MongoError({ message, cause }))),
-    Match.orElse((cause) => Stream.die(new Error(message, { cause })))
-  ))
--- packages/effect-mongodb/src/internal/mongo-error.ts
+++ packages/effect-mongodb/src/internal/mongo-error.ts
@@ -1,7 +1,6 @@
 import * as Effect from "effect/Effect"
 import * as F from "effect/Function"
 import * as Match from "effect/Match"
-import * as Stream from "effect/Stream"
 import { MongoError as MongoError_ } from "mongodb"
 import type { ErrorSource } from "../MongoError.js"
 import { MongoError } from "../MongoError.js"
@@ -12,14 +11,6 @@ export const mongoErrorOrDie =
       Match.value(error),
       Match.when(Match.instanceOf(MongoError_), F.flow(makeMongoError(source, message), Effect.fail)),
       Match.orElse(F.flow(makeError(source, message), Effect.die))
-    )
-
-export const mongoErrorOrDieStream =
-  (source: ErrorSource, message?: string) => (error: unknown): Stream.Stream<never, MongoError> =>
-    F.pipe(
-      Match.value(error),
-      Match.when(Match.instanceOf(MongoError_), F.flow(makeMongoError(source, message), Stream.fail)),
-      Match.orElse(F.flow(makeError(source, message), Stream.die))
     )
 
 const makeMongoError = (source: ErrorSource, message: string | undefined) => (cause: MongoError_) =>
