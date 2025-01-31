3fa084b236a51de1cb52b9ed3a426b6d904f3d38
Cosimo Matteini <dev.matteini@gmail.com>
2025-01-31 16:00:41 +0100

    services - DbService: add optional options to layer* functions (close #38)

--- packages/services/src/DbService.ts
+++ packages/services/src/DbService.ts
@@ -6,7 +6,7 @@ import type * as Brand from "effect/Brand"
 import * as Context from "effect/Context"
 import * as Effect from "effect/Effect"
 import * as Layer from "effect/Layer"
-import type { Db } from "mongodb"
+import type { Db, DbOptions } from "mongodb"
 import type * as MongoClientService from "./MongoClientService.js"
 
 export type DbService<K extends string> = Db & Brand.Brand<K>
@@ -17,23 +17,25 @@ export type TagType<K extends string> = ReturnType<typeof Tag<K>>
 export const layerEffect = <DbK extends string, MongoClientK extends string, E = never, R = never>(
   dbTag: TagType<DbK>,
   clientTag: MongoClientService.TagType<MongoClientK>,
-  dbName: Effect.Effect<string, E, R>
+  dbName: Effect.Effect<string, E, R>,
+  options?: DbOptions
 ) =>
   Effect.gen(function*() {
     const dbname_ = yield* dbName
-    return layer(dbTag, clientTag, dbname_)
+    return layer(dbTag, clientTag, dbname_, options)
   }).pipe(Layer.unwrapEffect)
 
 export const layer = <DbK extends string, MongoClientK extends string>(
   dbTag: TagType<DbK>,
   clientTag: MongoClientService.TagType<MongoClientK>,
-  dbName: string
+  dbName: string,
+  options?: DbOptions
 ) =>
   Layer.effect(
     dbTag,
     Effect.gen(function*() {
       const client = yield* clientTag
-      const db = MongoClient.db(client, dbName)
+      const db = MongoClient.db(client, dbName, options)
       return dbTag.of(db as DbService<DbK>) // TODO fix cast using branded ctor
     })
   )
