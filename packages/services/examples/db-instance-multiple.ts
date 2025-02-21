ee4dd248ffb679dad87023dbf50cb90254c2226e
Andrea Vallotti <andrea.vallotti@gmail.com>
2025-02-21 16:16:27 +0100

    services - examples: using DbInstance multiple times with different connection strings (#36)

---
+++ packages/services/examples/db-instance-multiple.ts
@@ -0,0 +1,48 @@
+import * as DbInstance from "@effect-mongodb/services/DbInstance"
+import * as DbService from "@effect-mongodb/services/DbService"
+import * as Collection from "effect-mongodb/Collection"
+import * as Db from "effect-mongodb/Db"
+import * as FindCursor from "effect-mongodb/FindCursor"
+import * as Effect from "effect/Effect"
+import * as Schema from "effect/Schema"
+
+const Todo = Schema.Struct({
+  userId: Schema.Number,
+  id: Schema.Number,
+  title: Schema.String,
+  completed: Schema.Boolean
+})
+
+const MainDb = DbService.Tag("MainDb")
+const ReplicaDb = DbService.Tag("ReplicaDb")
+
+const program = Effect.gen(function*() {
+  const mainDb = yield* MainDb
+  const newDb = yield* ReplicaDb
+
+  const collectionName = "source"
+  const mainCollection = Db.collection(mainDb, collectionName, Todo)
+  const replicaCollection = Db.collection(newDb, collectionName, Todo)
+
+  const items = yield* Collection.find(mainCollection).pipe(FindCursor.toArray)
+
+  yield* Collection.insertMany(replicaCollection, items)
+})
+
+/*** main.ts ***/
+
+const MainDbLive = DbInstance.layer(MainDb, {
+  database: { name: "mydb" },
+  client: { url: "mongodb://localhost:27017" }
+})
+
+const ReplicaDbLive = DbInstance.layer(ReplicaDb, {
+  database: { name: "mydb" },
+  client: { url: "mongodb://localhost:37017" }
+})
+
+await program.pipe(
+  Effect.provide(MainDbLive),
+  Effect.provide(ReplicaDbLive),
+  Effect.runPromise
+)
