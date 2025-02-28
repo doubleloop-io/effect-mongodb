6dbb3d717e37e5decc7ad75563d66192e45f96bf
Cosimo Matteini <dev.matteini@gmail.com>
2025-02-28 16:04:33 +0100

    services - MongoClientService: add explicit return type on public code + type tests (#44)

---
+++ packages/services/dtslint/MongoClientService.ts
@@ -0,0 +1,57 @@
+import * as MongoClientService from "@effect-mongodb/services/MongoClientService"
+import type * as MongoClient from "effect-mongodb/MongoClient"
+import * as Config from "effect/Config"
+import type * as Context from "effect/Context"
+import * as Effect from "effect/Effect"
+
+declare const mongoClient: MongoClientService.Tag<"MyMongoClient">
+
+type SomeService = { url: string }
+declare const SomeService: Context.Tag<SomeService, SomeService>
+
+// -------------------------------------------------------------------------------------
+// Tag
+// -------------------------------------------------------------------------------------
+
+// $ExpectType Tag<"MyMongoClient">
+MongoClientService.Tag("MyMongoClient")
+
+// -------------------------------------------------------------------------------------
+// layerEffect
+// -------------------------------------------------------------------------------------
+
+// $ExpectType Layer<MongoClientService<"MyMongoClient">, MongoError, never>
+MongoClientService.layerEffect(mongoClient, Effect.succeed("mongodb://localhost:27017"))
+
+// $ExpectType Layer<MongoClientService<"MyMongoClient">, ConfigError | MongoError, never>
+MongoClientService.layerEffect(mongoClient, Config.string("DATABASE_NAME"))
+
+const withRequirements = SomeService.pipe(Effect.flatMap(({ url }) => Config.string(url)))
+// $ExpectType Layer<MongoClientService<"MyMongoClient">, ConfigError | MongoError, SomeService>
+MongoClientService.layerEffect(mongoClient, withRequirements)
+
+// -------------------------------------------------------------------------------------
+// layer
+// -------------------------------------------------------------------------------------
+
+// $ExpectType Layer<MongoClientService<"MyMongoClient">, MongoError, never>
+MongoClientService.layer(mongoClient, "mongodb://localhost:27017")
+
+// -------------------------------------------------------------------------------------
+// fromMongoClient
+// -------------------------------------------------------------------------------------
+
+declare const legacyClient: Effect.Effect<MongoClient.MongoClient>
+
+// $ExpectType Layer<MongoClientService<"MyMongoClient">, never, never>
+MongoClientService.fromMongoClient(mongoClient, legacyClient)
+
+declare const legacyClientWithError: Effect.Effect<MongoClient.MongoClient, Error>
+
+// $ExpectType Layer<MongoClientService<"MyMongoClient">, Error, never>
+MongoClientService.fromMongoClient(mongoClient, legacyClientWithError)
+
+declare const legacyClientWithRequirements: Effect.Effect<MongoClient.MongoClient, Error, SomeService>
+
+// $ExpectType Layer<MongoClientService<"MyMongoClient">, Error, SomeService>
+MongoClientService.fromMongoClient(mongoClient, legacyClientWithRequirements)
--- packages/services/src/MongoClientService.ts
+++ packages/services/src/MongoClientService.ts
@@ -2,6 +2,7 @@
  * @since 0.0.1
  */
 import * as MongoClient from "effect-mongodb/MongoClient"
+import type * as MongoError from "effect-mongodb/MongoError"
 import type * as Brand from "effect/Brand"
 import * as Context from "effect/Context"
 import * as Effect from "effect/Effect"
@@ -16,7 +17,7 @@ export const layerEffect = <MongoClientK extends string, E = never, R = never>(
   clientTag: Tag<MongoClientK>,
   url: Effect.Effect<string, E, R>,
   options?: MongoClient.MongoClientScopedOptions
-) =>
+): Layer.Layer<MongoClientService<MongoClientK>, MongoError.MongoError | E, R> =>
   Effect.gen(function*() {
     const url_ = yield* url
     return layer(clientTag, url_, options)
@@ -26,7 +27,7 @@ export const layer = <MongoClientK extends string>(
   clientTag: Tag<MongoClientK>,
   url: string,
   options?: MongoClient.MongoClientScopedOptions
-) =>
+): Layer.Layer<MongoClientService<MongoClientK>, MongoError.MongoError> =>
   Layer.scopedContext(Effect.gen(function*() {
     const client = yield* MongoClient.connectScoped(url, options)
     return Context.make(clientTag, client as MongoClientService<MongoClientK>)
@@ -35,7 +36,7 @@ export const layer = <MongoClientK extends string>(
 export const fromMongoClient = <MongoClientK extends string, E = never, R = never>(
   clientTag: Tag<MongoClientK>,
   mongoClient: Effect.Effect<MongoClient.MongoClient, E, R>
-) =>
+): Layer.Layer<MongoClientService<MongoClientK>, E, R> =>
   Layer.effect(
     clientTag,
     Effect.map(mongoClient, (client) => client as MongoClientService<MongoClientK>)
